eb5df73679e92e09495e774b38ba4f607ebb2a85
JngMkk <kjm0817ss@gmail.com>
2024-04-28 21:32:01 +0900

    chore: move files

--- Scala/anonymousFunc_ex.scala
+++
@@ -1,8 +0,0 @@
-object AnonymousFunc {
-    def main(args: Array[String]): Unit = {
-        def TransferMoney(money: Double, bankFee: Double => Double): Unit = {
-            println(money + bankFee(money))
-        }
-        TransferMoney(100, (amount: Double) => amount * 0.05)
-    }
-}
